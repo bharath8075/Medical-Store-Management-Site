16b483cf8d4d6481177ed448f73c326b592a6a3a
Bharath <bharathr8075@gmail.com>
2025-02-22 10:52:04 +0530

    Third Commit

--- src/auth/checkAuth.js
+++ src/auth/checkAuth.js
@@ -1,10 +1,8 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export const checkAuth = (Component) => {
   function Wrapper(props) {
-    // var user = useSelector((store) => store.auth.currentUser);
     var user = localStorage.getItem("currentUser");
     var navigate = useNavigate();
     useEffect(() => {
